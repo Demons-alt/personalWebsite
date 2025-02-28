dae2669c51abd30a13210c731ee6c06042636f15
ahmadFahmie <ahmad.nurfahmi@dansmultipro.com>
2025-02-28 23:28:33 +0700

    make coming soon page

--- app/root.tsx
+++ app/root.tsx
@@ -6,9 +6,10 @@ import {
   Scripts,
   ScrollRestoration,
 } from "react-router";
-
 import type { Route } from "./+types/root";
 import "./app.css";
+import SplashCursor from "./components/splashCrusor";
+// import ThreeScene from "./3D/sample";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -19,7 +20,7 @@ export const links: Route.LinksFunction = () => [
   },
   {
     rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+    href: "https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap",
   },
 ];
 
@@ -32,7 +33,8 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="relative  overflow-hidden">
+        <SplashCursor />
         {children}
         <ScrollRestoration />
         <Scripts />
